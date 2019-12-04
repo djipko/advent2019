30826d6ebc2709ca8530077b565674e1eb2f7dcc
Nikola Đipanov <nikola.djipanov@gmail.com>
2019-12-04 19:39:58 +0000

    day 1 1

--- day1/src/Lib.hs
+++ day1/src/Lib.hs
@@ -1,5 +1,7 @@
 module Lib
     ( day1
+    , day1_1
+    
     ) where
 
 import Control.Monad
@@ -13,3 +15,18 @@ day1 = do
     file_data <- getModules
     let fuel = [f `div` 3 - 2 | f <- file_data]
     (putStrLn . show) (foldr (+) 0 fuel)
+
+fuel :: Integer -> Integer
+fuel mass = mass `div` 3 - 2
+
+massWFuel :: Integer -> Integer
+massWFuel mass = 
+    let fm = fuel mass
+    in if fm <= 0 then 0
+       else fm + massWFuel fm
+
+day1_1 :: IO ()
+day1_1 = do
+    file_data <- getModules
+    let mwf =[massWFuel m | m <- file_data]
+    (putStrLn . show) (foldr (+) 0 mwf)
