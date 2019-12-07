9cdfe4cece8271fcbc888dc253bca546f8cd1786
Nikola Đipanov <nikola.djipanov@gmail.com>
2019-12-07 21:57:02 +0000

    day 3 1

--- day3/src/Lib.hs
+++ day3/src/Lib.hs
@@ -1,10 +1,14 @@
 module Lib
     ( day3
+    , run
+    , Direction(..)
+    , Segment(..)
+    , manhattan
     ) where
 
 import Control.Monad
 import qualified Control.Monad.State as St
-import Data.List (sortOn)
+import Data.List (sortOn, elemIndices)
 import Data.Set (Set)
 import qualified Data.Set as Set
 import Text.Parsec
@@ -20,8 +24,9 @@ type Coord = (Int, Int)
 
 data WireRun = WireRun {
       coords :: Set Coord
+    , orderedCoords :: [Coord]
     , currentCoord :: Coord
-}
+} deriving Show
 
 direction :: Parsec String s Direction
 direction = do
@@ -51,10 +56,22 @@ readData :: IO String
 readData = readFile "input.txt"
 
 run :: Coord -> Segment -> [Coord]
-run start (Segment L dst) = zip (repeat $ fst start) [-1, (snd start)..(snd start) - dst -1]
-run start (Segment R dst) = zip (repeat $ fst start) [(snd start)..(snd start) + dst - 1]
-run start (Segment U dst) = zip [(fst start)..(fst start) + dst - 1] (repeat $ snd start) 
-run start (Segment D dst) = zip [-1, (fst start)..(fst start) - dst + 1] (repeat $ snd start) 
+run start (Segment L dst) =
+    let range_start = (fst start) - 1
+        range_end = range_start - dst + 1
+    in zip [range_start, (range_start-1)..range_end] (repeat $ snd start) 
+run start (Segment R dst) =
+    let range_start = (fst start) + 1
+        range_end = range_start + dst - 1
+    in zip [range_start..range_end] (repeat $ snd start) 
+run start (Segment U dst) =
+    let range_start = (snd start) + 1
+        range_end = range_start + dst - 1
+    in zip (repeat $ fst start) [range_start..range_end]  
+run start (Segment D dst) =
+    let range_start = (snd start) - 1
+        range_end = range_start - dst + 1
+    in zip (repeat $ fst start) [range_start, (range_start - 1)..range_end]  
 
 runWire :: Wire -> St.State WireRun ()
 runWire (segment : rst) = do
@@ -64,19 +81,26 @@ runWire (segment : rst) = do
         newRun = run start segment
     St.put $ WireRun{
         coords = currentCoords `Set.union` (Set.fromList newRun),
+        orderedCoords = (orderedCoords currentRun) ++ newRun,
         currentCoord = last newRun
     }
     runWire rst
 runWire [] = return ()
 
 emptyRun :: WireRun
-emptyRun = WireRun{coords=Set.empty, currentCoord=(0, 0)}
+emptyRun = WireRun{
+    coords=Set.singleton (0, 0),
+    orderedCoords=[(0, 0)],
+    currentCoord=(0, 0)
+}
 
 runWires :: [Wire] -> [WireRun]
 runWires = map $ (\w -> St.execState (runWire w) emptyRun)
 
+-- Currently deals only with first two
 crossingPoints :: [WireRun] -> [Coord]
-crossingPoints = Set.toList . foldr Set.intersection Set.empty . (map coords)
+crossingPoints (w1 : w2 : _) = Set.toList $ (coords w1) `Set.intersection` (coords w2)
+crossingPoints (_) = []
 
 manhattan :: Coord -> Coord -> Int
 manhattan (x1, y1) (x2, y2) = (abs $ x1 - x2) + (abs $ y1 - y2)
@@ -84,11 +108,18 @@ manhattan (x1, y1) (x2, y2) = (abs $ x1 - x2) + (abs $ y1 - y2)
 closest :: Coord -> ([Coord] -> Coord)
 closest start = head . (sortOn $ manhattan start) . filter (/= start)
 
+totalLatency :: Coord -> [WireRun] -> Int
+totalLatency cross = (foldr (+) 0) . (map $ head . elemIndices cross . orderedCoords)
+
 day3 :: IO ()
 day3 = do
     raw_data <- readData
     case (parse wires "" raw_data) of
         Left err -> print err
         Right ws -> do
+            let wr = runWires $ ws
             let getClosest = closest (0, 0) . crossingPoints . runWires 
+            let cs = filter (/= (0, 0)) $ crossingPoints wr
+            let latencies = map (flip totalLatency $ wr) cs
             print (manhattan (0, 0) $ getClosest ws)
+            print (minimum latencies)
