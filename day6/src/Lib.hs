ac6c47a7466aac42fe152021ed9c4aaf8c27295b
Nikola Đipanov <nikola.djipanov@gmail.com>
2019-12-10 00:39:35 +0000

    day 6 1

--- day6/src/Lib.hs
+++ day6/src/Lib.hs
@@ -1,13 +1,15 @@
 module Lib
     ( day6
+    , day6_1
     ) where
 
 import Control.Monad
 import Data.Foldable (toList)
+import Data.List
 import Data.List.Split
 import Data.Map.Strict (Map)
 import qualified Data.Map.Strict as Map
-import Data.Sequence (Seq, (|>))
+import Data.Sequence (Seq, (|>), (<|))
 import qualified Data.Sequence as Seq
 import Data.Tree
 
@@ -37,3 +39,22 @@ day6 = do
     let l = levels $ unfoldTree (nodeBuilder g) "COM"
     let f (d, l) s = s + d * length l
     print $ foldr f 0 $ zip [0..] l
+
+foldPath :: String -> String -> [Seq String] -> Seq String
+foldPath el x xs
+    | el == x = Seq.singleton x
+    | otherwise = case sortOn length $ map ((<|) x) $ filter (not . null) xs of
+        [] -> Seq.Empty
+        (s:_) -> s
+
+commonPrefix :: Seq String -> Seq String -> Int
+commonPrefix = curry $ Seq.length . Seq.takeWhileL (\(x, y) -> x == y) . (uncurry Seq.zip)
+
+day6_1 :: IO ()
+day6_1 = do
+    g <- loadGraph
+    let tree = unfoldTree (nodeBuilder g) "COM"
+    let you_path = foldTree (foldPath "YOU") tree
+    let san_path = foldTree (foldPath "SAN") tree
+    let common = commonPrefix san_path you_path
+    print $ ((length you_path) - common) + ((length san_path) - common) - 2
