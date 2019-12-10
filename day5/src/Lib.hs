2cf5a772b2126cbde324e91467775a337f47f1cf
Nikola Đipanov <nikola.djipanov@gmail.com>
2019-12-10 21:19:36 +0000

    day5 1

--- day5/src/Lib.hs
+++ day5/src/Lib.hs
@@ -1,5 +1,6 @@
 module Lib
     ( day5
+    , day5_1
     , runProgram
     , Program(..)
     , parseOpcode
@@ -35,16 +36,26 @@ data Op =
     | Mul Param Param Param
     | Input Int
     | Output Param
+    | JumpIfTrue Param Param
+    | JumpIfFalse Param Param
+    | LessThan Param Param Param
+    | Equals Param Param Param
     | End deriving Show
 
-threeParamOps = ['1', '2']
-
 oneParamOps = ['4']
+
+twoParamOps = ['5', '6']
+
+threeParamOps = ['1', '2', '7', '8']
+
 
 parseExtended digit modes
         | digit `elem` threeParamOps =
             -- see Traversable instance of (,) a to see why this works
             sequence ((read [digit]), (parseModes_ 3 modes))
+        | digit `elem` twoParamOps =
+            -- see Traversable instance of (,) a to see why this works
+            sequence ((read [digit]), (parseModes_ 2 modes))
         | digit `elem` oneParamOps =
             -- see Traversable instance of (,) a to see why this works
             sequence ((read [digit]), (parseModes_ 1 modes))
@@ -53,6 +64,8 @@ parseExtended digit modes
 parseBase digit
         | digit `elem` threeParamOps =
             return ((read [digit]) , replicate 3 PosMode)
+        | digit `elem` twoParamOps =
+            return ((read [digit]) , replicate 2 PosMode)
         | digit `elem` oneParamOps =
             return ((read [digit]) , replicate 1 PosMode)
         | otherwise = Nothing
@@ -72,20 +85,29 @@ parseModes_ paramCnt chrs =
         res = take paramCnt $ (map mode chrs) ++ (repeat $ Just PosMode)
     in sequence res
 
-opFromParams :: (Param -> Param -> Param -> Op) -> [Param] -> Op
-opFromParams opC params = opC (params !! 0) (params !! 1) (params !! 2)
+opFromParams3 :: (Param -> Param -> Param -> Op) -> [Param] -> Op
+opFromParams3 opC params = opC (params !! 0) (params !! 1) (params !! 2)
+
+opFromParams2 :: (Param -> Param -> Op) -> [Param] -> Op
+opFromParams2 opC params = opC (params !! 0) (params !! 1)
+
+opFromParams :: (Param -> Op) -> [Param] -> Op
+opFromParams opC params = opC (params !! 0)
 
 parseOp :: Program -> Maybe (Op, Int)
 parseOp (Running (99 : _) _ _) = Just (End, 0)
 parseOp (Running (3 : ptr : _) _ _) = Just (Input ptr, 2)
 parseOp (Running (opcode : rst) _ _) = do
-    let threeParams = map (uncurry Param) . zip (take 3 rst)
-    let oneParam modes = Param (head rst) (head modes)
+    let buildParams n = map (uncurry Param) . zip (take n rst)
     parsed <- parseOpcode opcode
     case parsed of
-        (1, modes) -> Just ((opFromParams Add (threeParams modes)), 4)
-        (2, modes) -> Just ((opFromParams Mul (threeParams modes)), 4)
-        (4, modes) -> Just ((Output $ oneParam modes), 2)
+        (1, modes) -> Just ((opFromParams3 Add (buildParams 3 modes)), 4)
+        (2, modes) -> Just ((opFromParams3 Mul (buildParams 3 modes)), 4)
+        (4, modes) -> Just ((opFromParams Output (buildParams 1 modes)), 2)
+        (5, modes) -> Just ((opFromParams2 JumpIfTrue (buildParams 2 modes)), 3)
+        (6, modes) -> Just ((opFromParams2 JumpIfFalse (buildParams 2 modes)), 3)
+        (7, modes) -> Just ((opFromParams3 LessThan (buildParams 3 modes)), 4)
+        (8, modes) -> Just ((opFromParams3 Equals (buildParams 3 modes)), 4)
         (_) -> Nothing
 parseOp (_) = Nothing
 
@@ -97,22 +119,41 @@ fetchOp mem op = case op of
 fetchOpVal :: Param -> Int
 fetchOpVal (Param val _) = val
 
-executeOp :: Program -> Op -> Maybe Program
+executeOp :: Program -> Op -> Maybe (Program, Maybe Int)
 executeOp (Running mem i o) (Add fst snd ptr) = 
     let res = fetchOp mem fst + fetchOp mem snd
         (beg, end) = splitAt (fetchOpVal ptr) mem
-    in Just $ Running (beg ++ [res] ++ tail end) i o
+    in Just ((Running (beg ++ [res] ++ tail end) i o), Nothing)
 executeOp (Running mem i o) (Mul fst snd ptr) = 
     let res = fetchOp mem fst * fetchOp mem snd
         (beg, end) = splitAt (fetchOpVal ptr) mem
-    in Just $ Running (beg ++ [res] ++ tail end) i o
+    in Just $ ((Running (beg ++ [res] ++ tail end) i o), Nothing)
 executeOp (Running mem i o) (Input ptr) = do
     input <- i !? 0
-    Just $ Running (beg ++ [input] ++ tail end) (Sq.drop 1 i) o
+    Just ((Running (beg ++ [input] ++ tail end) (Sq.drop 1 i) o), Nothing)
     where
         (beg, end) = splitAt ptr mem
-executeOp (Running mem i o) (Output fst) = Just $ Running mem i $ (fetchOp mem fst) <| o
-executeOp (Running mem i o) End = Just (Halted mem o)
+executeOp (Running mem i o) (Output fst) =
+    Just ((Running mem i $ (fetchOp mem fst) <| o), Nothing)
+executeOp (Running mem i o) (JumpIfTrue fst snd) =
+    if (fetchOp mem fst /= 0)
+        then Just ((Running mem i o), Just $ fetchOp mem snd)
+        else Just ((Running mem i o), Nothing)
+executeOp (Running mem i o) (JumpIfFalse fst snd) =
+    if (fetchOp mem fst == 0)
+        then Just ((Running mem i o), Just $ fetchOp mem snd)
+        else Just ((Running mem i o), Nothing)
+executeOp (Running mem i o) (LessThan fst snd ptr) =
+    Just ((Running (beg ++ [val] ++ tail end) i o), Nothing)
+    where
+        val = if (fetchOp mem fst) < (fetchOp mem snd) then 1 else 0
+        (beg, end) = splitAt (fetchOpVal ptr) mem
+executeOp (Running mem i o) (Equals fst snd ptr) =
+    Just ((Running (beg ++ [val] ++ tail end) i o), Nothing)
+    where
+        val = if (fetchOp mem fst) == (fetchOp mem snd) then 1 else 0
+        (beg, end) = splitAt (fetchOpVal ptr) mem
+executeOp (Running mem i o) End = Just ((Halted mem o), Nothing)
 
 rewindProgram :: Program -> Int -> Program
 rewindProgram (Running prog i o) ptr = Running (drop ptr prog) i o
@@ -128,13 +169,15 @@ interpret_ ptr = do
                 Nothing -> put $ Error mem ptr
                 Just (prog, jmp) -> do
                     put prog 
-                    interpret_ (ptr + jmp)
+                    interpret_ jmp
         Error mem _ -> put $ Error mem ptr
     where 
         execute_ prog = do
-            (op, jmp) <- parseOp (rewindProgram prog ptr) 
+            (op, op_len) <- parseOp (rewindProgram prog ptr) 
             mod_prog <- executeOp prog op
-            return (mod_prog, jmp)
+            case mod_prog of
+                (p, Nothing) -> return (p, ptr + op_len)
+                (p, Just new_ptr) -> return (p, new_ptr)
 
 loadProgram :: Seq Int -> IO Program
 loadProgram input = do
@@ -150,3 +193,6 @@ validateResult (error) = print error
 
 day5 :: IO ()
 day5 = loadProgram (Sq.singleton 1) >>= runProgram >>= validateResult
+
+day5_1 :: IO ()
+day5_1 = loadProgram (Sq.singleton 5) >>= runProgram >>= validateResult
