62be407ad45e668704730c6fe07b49eb8cdd08fa
Nikola Đipanov <nikola.djipanov@gmail.com>
2019-12-13 20:23:10 +0000

    day 8 1

--- day8/app/Main.hs
+++ day8/app/Main.hs
@@ -3,4 +3,4 @@ module Main where
 import Lib
 
 main :: IO ()
-main = day8
+main = day8 >> day8_1
--- day8/src/Lib.hs
+++ day8/src/Lib.hs
@@ -1,5 +1,9 @@
 module Lib
     ( day8
+    , day8_1
+    , loadImage
+    , splitLayers
+    , mergeLayers
     ) where
 
 import Data.List
@@ -19,8 +23,23 @@ count ch = length . filter (== ch)
 
 lowest ch layers = head $ sortOn snd $ zip layers $ map (count ch) layers
 
+mergeLayers :: [String] -> String
+mergeLayers layers = 
+    (head $ (dropWhile (== '2')) pixels) : doRest
+    where 
+        rest = map tail layers
+        doRest = if null . sequence $ rest
+            then []
+            else mergeLayers rest
+        pixels = map head layers
+
 day8 :: IO ()
 day8 =
     loadImage >>= print . (\l -> (count '2' l) * (count '1' l)) . layer
     where 
         layer = fst . lowest '0' . splitLayers
+
+day8_1 = do
+    loadImage >>= mapM_ putStrLn . (divvy width width) . map replace . mergeLayers . splitLayers
+    where
+        replace c = if c /= '0' then c else ' '
