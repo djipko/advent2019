c88a1e698d0f8e9c5ac32b5cc94961c392a328c2
Nikola Đipanov <nikola.djipanov@gmail.com>
2019-12-13 17:57:24 +0000

    day7 1

--- day7/app/Main.hs
+++ day7/app/Main.hs
@@ -3,4 +3,4 @@ module Main where
 import Lib
 
 main :: IO ()
-main = day7
+main = day7 >> day7_1
--- day7/src/Lib.hs
+++ day7/src/Lib.hs
@@ -1,16 +1,22 @@
 module Lib
     ( day7
+    , day7_1
     , runProgram
     , Program(..)
     , parseOpcode
     , parseOp
+    , generateOutput
+    , getLastOutput
+    , step
+    , passInput
+    , ampsFb
     ) where
 
 import Control.Monad
 import Control.Monad.State
 import Data.List
 import Data.List.Split
-import Data.Sequence (Seq, (<|), (!?))
+import Data.Sequence (Seq, (<|), (!?), (|>))
 import qualified Data.Sequence as Seq
 
 type Memory = [Int]
@@ -19,9 +25,15 @@ type Input = Seq Int
 
 type Output = Seq Int
 
-data Program = Running Memory Input Output | Halted Memory Output | Error Memory Int deriving Show
+type Pc = Int
+
+data Program = Running Memory Input Output Pc
+    | Halted Memory Output
+    | Error Memory Pc deriving Show
 
 type RunningProgram = State Program ()
+
+type OutProgram = State Program (Maybe Int)
 
 data ParamMode = PosMode | ImmediateMode deriving Show
 
@@ -47,7 +59,6 @@ oneParamOps = ['4']
 twoParamOps = ['5', '6']
 
 threeParamOps = ['1', '2', '7', '8']
-
 
 parseExtended digit modes
         | digit `elem` threeParamOps =
@@ -95,9 +106,9 @@ opFromParams :: (Param -> Op) -> [Param] -> Op
 opFromParams opC params = opC (params !! 0)
 
 parseOp :: Program -> Maybe (Op, Int)
-parseOp (Running (99 : _) _ _) = Just (End, 0)
-parseOp (Running (3 : ptr : _) _ _) = Just (Input ptr, 2)
-parseOp (Running (opcode : rst) _ _) = do
+parseOp (Running (99 : _) _ _ _) = Just (End, 0)
+parseOp (Running (3 : ptr : _) _ _ _) = Just (Input ptr, 2)
+parseOp (Running (opcode : rst) _ _ _) = do
     let buildParams n = map (uncurry Param) . zip (take n rst)
     parsed <- parseOpcode opcode
     case parsed of
@@ -120,79 +131,121 @@ fetchOpVal :: Param -> Int
 fetchOpVal (Param val _) = val
 
 executeOp :: Program -> Op -> Maybe (Program, Maybe Int)
-executeOp (Running mem i o) (Add fst snd ptr) = 
+executeOp (Running mem i o pc) (Add fst snd ptr) = 
     let res = fetchOp mem fst + fetchOp mem snd
         (beg, end) = splitAt (fetchOpVal ptr) mem
-    in Just ((Running (beg ++ [res] ++ tail end) i o), Nothing)
-executeOp (Running mem i o) (Mul fst snd ptr) = 
+    in Just ((Running (beg ++ [res] ++ tail end) i o pc), Nothing)
+executeOp (Running mem i o pc) (Mul fst snd ptr) = 
     let res = fetchOp mem fst * fetchOp mem snd
         (beg, end) = splitAt (fetchOpVal ptr) mem
-    in Just $ ((Running (beg ++ [res] ++ tail end) i o), Nothing)
-executeOp (Running mem i o) (Input ptr) = do
+    in Just $ ((Running (beg ++ [res] ++ tail end) i o pc), Nothing)
+executeOp (Running mem i o pc) (Input ptr) = do
     input <- i !? 0
-    Just ((Running (beg ++ [input] ++ tail end) (Seq.drop 1 i) o), Nothing)
+    Just ((Running (beg ++ [input] ++ tail end) (Seq.drop 1 i) o pc), Nothing)
     where
         (beg, end) = splitAt ptr mem
-executeOp (Running mem i o) (Output fst) =
-    Just ((Running mem i $ (fetchOp mem fst) <| o), Nothing)
-executeOp (Running mem i o) (JumpIfTrue fst snd) =
+executeOp (Running mem i o pc) (Output fst) =
+    Just ((Running mem i ((fetchOp mem fst) <| o) pc), Nothing)
+executeOp (Running mem i o pc) (JumpIfTrue fst snd) =
     if (fetchOp mem fst /= 0)
-        then Just ((Running mem i o), Just $ fetchOp mem snd)
-        else Just ((Running mem i o), Nothing)
-executeOp (Running mem i o) (JumpIfFalse fst snd) =
+        then Just ((Running mem i o pc), Just $ fetchOp mem snd)
+        else Just ((Running mem i o pc), Nothing)
+executeOp (Running mem i o pc) (JumpIfFalse fst snd) =
     if (fetchOp mem fst == 0)
-        then Just ((Running mem i o), Just $ fetchOp mem snd)
-        else Just ((Running mem i o), Nothing)
-executeOp (Running mem i o) (LessThan fst snd ptr) =
-    Just ((Running (beg ++ [val] ++ tail end) i o), Nothing)
+        then Just ((Running mem i o pc), Just $ fetchOp mem snd)
+        else Just ((Running mem i o pc), Nothing)
+executeOp (Running mem i o pc) (LessThan fst snd ptr) =
+    Just ((Running (beg ++ [val] ++ tail end) i o pc), Nothing)
     where
         val = if (fetchOp mem fst) < (fetchOp mem snd) then 1 else 0
         (beg, end) = splitAt (fetchOpVal ptr) mem
-executeOp (Running mem i o) (Equals fst snd ptr) =
-    Just ((Running (beg ++ [val] ++ tail end) i o), Nothing)
+executeOp (Running mem i o pc) (Equals fst snd ptr) =
+    Just ((Running (beg ++ [val] ++ tail end) i o pc), Nothing)
     where
         val = if (fetchOp mem fst) == (fetchOp mem snd) then 1 else 0
         (beg, end) = splitAt (fetchOpVal ptr) mem
-executeOp (Running mem i o) End = Just ((Halted mem o), Nothing)
+executeOp (Running mem i o pc) End = Just ((Halted mem o), Nothing)
 
-rewindProgram :: Program -> Int -> Program
-rewindProgram (Running prog i o) ptr = Running (drop ptr prog) i o
-rewindProgram (non_running) ptr = non_running
+rewindProgram :: Program -> Program
+rewindProgram (Running prog i o pc) = Running (drop pc prog) i o 0
+rewindProgram (non_running) = non_running
 
-interpret_ :: Int -> RunningProgram
-interpret_ ptr = do
+movePc :: Program -> Int -> Program
+movePc (Running prog i o pc) new_pc = Running prog i o new_pc
+movePc (non_running) ptr = non_running
+
+getPc :: Program -> Maybe Int
+getPc (Running prog i o pc) = Just pc
+getPc (non_running) = Nothing
+
+step :: RunningProgram
+step = do
     prog <- get
     case prog of
-        Halted mem o -> put $ Halted mem o   -- should never happen
-        Running mem i o -> 
+        Halted mem o -> put $ Halted mem o
+        Running mem i o pc -> 
             case execute_ prog of
-                Nothing -> put $ Error mem ptr
-                Just (prog, jmp) -> do
-                    put prog 
-                    interpret_ jmp
-        Error mem _ -> put $ Error mem ptr
+                Nothing -> put $ Error mem pc
+                Just (prog, jmp) -> put $ movePc prog jmp
+        Error mem pc -> put $ Error mem pc
     where 
         execute_ prog = do
-            (op, op_len) <- parseOp (rewindProgram prog ptr) 
+            ptr <- getPc prog
+            (op, op_len) <- parseOp (rewindProgram prog) 
             mod_prog <- executeOp prog op
             case mod_prog of
                 (p, Nothing) -> return (p, ptr + op_len)
-                (p, Just new_ptr) -> return (p, new_ptr)
+                (p, Just new_ptr) -> return (p, new_ptr) 
+
+runUntil :: (Program -> Bool) -> RunningProgram
+runUntil pred = do
+    prog <- get
+    if pred prog
+        then return ()
+        else step >> (runUntil pred)
+
+interpret :: RunningProgram
+interpret = runUntil done_
+    where
+        done_ (Halted m o) = True
+        done_ (Error m ptr) = True
+        done_ _ = False
+
 
 loadProgram :: IO Memory
 loadProgram = liftM ((map read) . splitOn ",") . readFile $ "input.txt"
 
 initAmp :: Memory -> Int -> Int -> Program
-initAmp mem phase input = Running mem (Seq.fromList [phase, input]) Seq.empty
+initAmp mem phase input = Running mem (Seq.fromList [phase, input]) Seq.empty 0
+
+initAmpFb :: Memory -> Int -> Program
+initAmpFb mem phase = Running mem (Seq.fromList [phase]) Seq.empty 0
+
+initAmpA :: Memory -> Int -> Program
+initAmpA mem phase = Running mem (Seq.fromList [phase, 0]) Seq.empty 0
 
 runProgram :: Program -> Program
-runProgram program = execState (interpret_ 0) program
+runProgram program = execState interpret program
+
+passInput :: Int -> Program -> Program
+passInput val (Running m i o pc) = Running m (i |> val) o pc
+passInput val prog = prog
 
 readOutput :: Program -> Maybe Int
 readOutput (Halted mem o) = o !? 0
 readOutput (_) = Nothing
 
+getLastOutput :: Program -> Maybe Int
+getLastOutput (Running mem i o pc) = o !? 0
+getLastOutput (Halted mem o) = o !? 0
+getLastOutput (_) = Nothing
+
+dropOutput :: Program -> Program
+dropOutput (Running mem i o pc) = Running mem i Seq.empty pc
+dropOutput prog = prog
+
 type Amp = Int -> Int -> Program
+type AmpFb = Program
 
 runPhase_ :: Int -> [Amp] -> [Int] -> Maybe Int
 runPhase_ input (amp:amps) (ph:phs) = do
@@ -203,6 +256,32 @@ runPhase_ input _ [] = Just input
 amps :: Memory -> [Amp]
 amps mem = take 5 . repeat $ (initAmp mem)
 
+ampsFb :: Memory -> [Int] -> [AmpFb]
+ampsFb mem (p:ps) = (execState (generateOutput 0) (initAmpFb mem p)) : map (initAmpFb mem) ps
+
+generateOutput :: Int -> OutProgram
+generateOutput input = do
+    modify' (passInput input)
+    runUntil output_
+    prog <- get
+    case prog of
+        Running m i o pc -> return $ o !? 0
+        Halted m o -> return $ o !? 0
+    where
+        output_ (Running m i o pc) = not . null $ o
+        output_ _ = True
+
+runPhaseFb :: [AmpFb] -> Maybe Int
+runPhaseFb (fst : snd : rst) = 
+    case getLastOutput fst of
+        Nothing -> Nothing
+        Just val ->
+            case (out, snd') of
+                (_, (Running m i o pc)) -> runPhaseFb ([snd'] ++ rst ++ [fst])
+                (_, (Halted m o)) -> Just val
+            where
+                (out, snd') = runState (generateOutput val) (dropOutput snd)
+
 day7 :: IO ()
 day7 = do
     mem <- loadProgram
@@ -211,3 +290,12 @@ day7 = do
         Nothing -> print "Error"
     where
         thrusts mem = map (runPhase_ 0 (amps mem)) $ permutations [0..4]
+
+day7_1 :: IO ()
+day7_1 = do
+    mem <- loadProgram
+    case liftM maximum $ sequence (thrusts mem) of
+        Just max -> print max
+        Nothing -> print "Error"
+    where
+        thrusts mem = map runPhaseFb $ map (ampsFb mem) $ permutations [5..9]
